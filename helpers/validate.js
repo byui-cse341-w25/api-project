4d02407350568b5cef86bf89e48c0a28d5960d42
Rubinel Youan Bi <104086734+Youan22@users.noreply.github.com>
2025-04-07 09:19:08 -0600

    added validate function

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,1 +1,9 @@
-const validator = require("../helpers/validate");
+const Validator = require("validatorjs");
+
+const validator = (body, rules, customMessages, callback) => {
+  const validation = new Validator(body, rules, customMessages);
+  validation.passes(() => callback(null, true));
+  validation.fails(() => callback(validation.errors, false)); 
+};
+
+module.exports = validator;
---
+++ middleware/validate.js
@@ -0,0 +1,28 @@
+const validator = require("../helpers/validate");
+
+const saveContact = (req, res, next) => {
+  const validationRule = {
+    title: "required|string",
+    author: "required|string",
+    isbn: "required|string",
+    publishedDate: "required|string",
+    genre: "required|string",
+    pages: "required|string",
+    language: "required|string",
+  };
+
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      return res.status(412).send({
+        success: false,
+        message: "Validation failed",
+        data: err,
+      });
+    }
+    next();
+  });
+};
+
+module.exports = {
+  saveContact,
+};
